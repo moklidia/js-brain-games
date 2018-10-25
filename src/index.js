56aa80f2fd789e85ccf515f64cee9013f7f5f447
moklidia <kitsunegakkou@gmail.com>
2018-10-25 13:18:24 +0300

    added brain-even

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import readlineSync from 'readline-sync';
+import getRandomNum from './numbers';
 
-const welcome = () => {
+const even = () => {
   console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
 
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello ${userName}!`);
+
+  const rounds = 3;
+
+  for (let actualRound = 1; actualRound <= rounds; actualRound += 1) {
+    const num = getRandomNum();
+
+    const question = console.log(`Question: ${num}`);
+
+    const userAnswer = readlineSync.question('Your answer:');
+
+    const rightAnswer = (num % 2 === 0) ? 'yes' : 'no';
+
+    if (userAnswer === rightAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+    console.log(`Congratulations, ${userName}!`);
 };
 
-export default welcome;
+export default even;
---
+++ src/numbers.js
@@ -0,0 +1,3 @@
+const getRandomNum = () => Math.floor(Math.random() * 100);
+
+export default getRandomNum;
