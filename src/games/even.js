5e6112b2b25d5326d9377587a88d235c839c461f
moklidia <kitsunegakkou@gmail.com>
2018-10-26 23:58:54 +0300

    another attempt

--- src/games/even.js
+++ src/games/even.js
@@ -4,14 +4,12 @@ import { getRandomNum } from '../numbers';
 
 const task = 'Answer "yes" if number even otherwise answer "no".';
 
-const getQuestion = getRandomNum();
-
 const isEven = num => (num % 2 === 0);
 
-const getRightAnswer = isEven(getQuestion) ? 'yes' : 'no';
-
-const gameData = cons(getQuestion, getRightAnswer);
-
-const playEven = () => playGame(task, gameData);
-
+const playRound = () => {
+  const getQuestion = getRandomNum();
+  const getRightAnswer = isEven(getQuestion) ? 'yes' : 'no';
+  return cons(getQuestion, getRightAnswer);
+};
+const playEven = () => playGame(task, playRound());
 export default playEven;
