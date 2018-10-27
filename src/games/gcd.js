3b1663beb842c07518e6773c723d7bd51c011252
moklidia <kitsunegakkou@gmail.com>
2018-10-27 14:09:04 +0300

    added brain-gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,24 @@
+import { cons } from 'hexlet-pairs';
+import playGame from '../game-flow';
+import { getRandomNum } from '../numbers';
+
+const task = 'Find the greatest common divisor of given numbers.';
+
+const num1 = getRandomNum();
+const num2 = getRandomNum();
+const getGCD = (a, b) => {
+  if (b === 0) {
+    return Math.abs(a);
+  }
+  return getGCD(b, a % b);
+};
+
+const playRound = () => {
+  const getQuestion = `${num1} ${num2}`;
+  const getRightAnswer = getGCD(num1, num2);
+  return cons(getQuestion, getRightAnswer);
+};
+
+const playGcd = () => playGame(task, playRound());
+
+export default playGcd;
