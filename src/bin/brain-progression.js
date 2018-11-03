4c994cda117364135601b31ea3af2858eef6f4ab
moklidia <kitsunegakkou@gmail.com>
2018-11-03 18:04:37 +0400

    added brain-progression

---
+++ src/bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import playProgression from '../games/progression';
+
+playProgression();
---
+++ src/games/progression.js
@@ -0,0 +1,28 @@
+import { cons } from 'hexlet-pairs';
+import playGame from '../game-flow';
+import getRandomNum from '../utils';
+
+const task = '';
+
+const playRound = () => {
+  const num = getRandomNum();
+  const step = Math.floor(Math.random() * 10);
+  const length = 10;
+  const progression = [];
+  for (let i = 1; i <= length; i += 1) {
+    progression.push(num + i * step);
+  }
+  const randomIndex = Math.floor(Math.random() * 10);
+  const rightAnswer = progression[randomIndex];
+  progression[randomIndex] = '..';
+
+  const question = `What number is missing in this progression?
+${progression.join(' ')}`;
+
+
+  return cons(question, rightAnswer);
+};
+
+const playCalc = () => playGame(task, playRound);
+
+export default playCalc;
